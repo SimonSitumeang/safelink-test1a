1cda10ec087d95790b0a740ec64f2aadfffb276e
SimonSitumeang <36192405+SimonSitumeang@users.noreply.github.com>
2018-12-22 15:05:46 +0700

    Update Pemanggil.js

--- Pemanggil.js
+++ Pemanggil.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="https://wowdor.blogspot.com/2018/12/direct-1.html"}
 if(x=="2"){xxx="https://wowdor.blogspot.com/2018/12/direct-2.html"}
 if(x=="3"){xxx="https://wowdor.blogspot.com/2018/12/direct-3.html"}
 if(x=="4"){xxx="https://wowdor.blogspot.com/2018/12/direct-4.html"}
-if(x=="5"){xxx="https://wowdor.blogspot.com/2018/12/direct-5.html"}a_to_ve[i].href="https://wowdor.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="https://wowdor.blogspot.com/2018/12/direct-5.html"}a_to_ve[i].href=""+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
